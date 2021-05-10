5bd75e4bc8a6777830d9b8e80773d33a64ddd4a1
luvagu <luiavag@gmail.com>
2021-05-10 18:14:42 -0300

    added value state

--- src/components/Cell.jsx
+++ src/components/Cell.jsx
@@ -1,9 +1,11 @@
-function Cell({ value }) {
-    return (
-        <div className="cell">
-            {value}
-        </div>
-    )
+import { useState } from 'react'
+
+function Cell({}) {
+	const [value, setValue] = useState(null)
+
+	return (
+		<div className={`cell ${value ? value : ''}`} onClick={() => setValue('x')} />
+	)
 }
 
 export default Cell
