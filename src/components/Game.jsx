434d4570446a9586eb2a92c57058728871bb05f9
luvagu <luiavag@gmail.com>
2021-05-11 13:14:42 -0300

    history now displays move location, changed styles

--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -1,41 +1,42 @@
 import { Fragment, useState } from 'react'
-import { calculateWinner, checkDraw } from '../helpers'
+import { calculateWinner, checkDraw, calculateMoveLocation } from '../helpers'
 import Board from './Board'
 import Overlay from './Overlay'
 
 function Game() {
-	const [history, setHistory] = useState([{ cells: new Array(9).fill(null) }])
+	const [history, setHistory] = useState([{ cells: new Array(9).fill(null), moveLocation: '' }])
 	const [moveNumber, setMoveNumber] = useState(0)
 	const [isXTurn, setIsXTurn] = useState(true)
-    const [isSelectedMove, setIsSelectedMove] = useState(null)
-    const [isClosed, setIsClosed] = useState(true)
+	const [isSelectedMove, setIsSelectedMove] = useState(null)
+	const [isClosed, setIsClosed] = useState(true)
 	const { cells } = history[moveNumber]
 	const { winner, wCells } = calculateWinner(cells)
-    const draw = checkDraw(cells)
-    const player = isXTurn ? 'x' : 'o'
+	const isDraw = checkDraw(cells)
+	const player = isXTurn ? 'x' : 'o'
 
 	const status = winner
 		? `Winner: ${winner.toUpperCase()}`
-        : draw ? 'Cats game!'
+		: isDraw
+		? 'Cats game!'
 		: `Next player: ${player.toUpperCase()}`
 
 	const handleClick = (index) => {
 		const newHistory = history.slice(0, moveNumber + 1)
 		const newCells = [...cells]
+		const moveLocation = calculateMoveLocation(index)
 		if (winner || newCells[index]) return
 		newCells[index] = player
-		setHistory([...newHistory, { cells: newCells }])
+		setHistory([...newHistory, { cells: newCells, moveLocation }])
 		setMoveNumber(newHistory.length)
 		setIsXTurn(!isXTurn)
-        setIsSelectedMove(null)
-        setIsClosed(true)
+		setIsSelectedMove(null)
+		setIsClosed(true)
 	}
 
 	const goToMove = (move) => {
 		setMoveNumber(move)
-        setIsXTurn(move % 2 === 0)
-        setIsSelectedMove(move)
-        // setIsClosed(winner ? false : true)
+		setIsXTurn(move % 2 === 0)
+		setIsSelectedMove(move)
 	}
 
 	return (
@@ -43,11 +44,15 @@ function Game() {
 			<div className="floating-panel">
 				<p>{status}</p>
 				<ol>
-					{history.map((step, move) => (
+					{history.map(({ cells, moveLocation }, move) => (
 						<li key={move}>
-							<button className={isSelectedMove === move ? 'selected' : ''} onClick={() => goToMove(move)}>
+							<button
+								className={isSelectedMove === move ? 'selected' : ''}
+								onClick={() => goToMove(move)}
+							>
 								{move ? `Go to move #${move}` : `Go to game start`}
 							</button>
+							<span>{moveLocation && moveLocation}</span>
 						</li>
 					))}
 				</ol>
@@ -60,7 +65,9 @@ function Game() {
 				handleClick={handleClick}
 			/>
 
-            {(winner || draw) && isClosed && <Overlay message={status} close={() => setIsClosed(false)} />}
+			{(winner || isDraw) && isClosed && (
+				<Overlay message={status} close={() => setIsClosed(false)} />
+			)}
 		</Fragment>
 	)
 }
--- src/helpers.js
+++ src/helpers.js
@@ -29,6 +29,24 @@ export const calculateWinner = (cells) => {
 
 export const checkDraw = (cells) => cells.every((cell) => cell !== null)
 
-export const calculateMovePosition = (index) => {
+export const calculateMoveLocation = (moveNumber) => {
+	if (typeof moveNumber !== 'number') return null
 
+	const table3By3 = [
+		[0, 1, 2],
+		[3, 4, 5],
+		[6, 7, 8],
+	]
+
+	let rowNum = 1,
+		colNum = null
+
+	for (const row of table3By3) {
+		if (row.indexOf(moveNumber) > -1) {
+			colNum = row.indexOf(moveNumber) + 1
+			break
+		}
+		rowNum++
+	}
+	return `pos: (${rowNum}, ${colNum})`
 }
