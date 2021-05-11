9f839df2322d05b15519a9bec5bb689686ce953a
luvagu <luiavag@gmail.com>
2021-05-10 21:04:24 -0300

    added moves history steps

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -1,56 +1,12 @@
-import { Fragment, useState } from 'react'
 import Cell from './Cell'
 
-const calculateWinner = (cells) => {
-    const Combinations = [
-        [0, 1, 2],
-        [3, 4, 5],
-        [6, 7, 8],
-        [0, 3, 6],
-        [1, 4, 7],
-        [2, 5, 8],
-        [0, 4, 8],
-        [2, 4, 6]    
-    ]
-
-    for (const combination of Combinations) {
-        const [a, b, c] = combination
-
-        if (cells[a] && cells[a] === cells[b] && cells[a] === cells[c]) {
-            return cells[a]
-        }
-    }
-
-    return null
-}
-
-function Board() {
-	const [cells, setCells] = useState(new Array(9).fill(null))
-    const [currentPlayer, setCurrentPlayer] = useState('x')
-    const [history, setHistory] = useState([])
-    const winner = calculateWinner(cells)
-	const status = winner ? `Winner: ${winner}` : `Next player: ${currentPlayer.toUpperCase()}`
-
-	const handleClick = (index) => {
-        setHistory({ cells })
-		const newCells = [...cells]
-        if (winner || newCells[index]) return
-        newCells[index] = currentPlayer
-		setCells(newCells)
-        setCurrentPlayer(currentPlayer === 'x' ? 'o' : 'x')
-	}
-
-	console.log({cells, history})
-
+function Board({ cells, currentPlayer, handleClick }) {
 	return (
-		<Fragment>
-			<div className="">{status}</div>
-			<main className={`game-grid ${currentPlayer}`}>
-				{cells.map((value, index) => (
-					<Cell key={index} value={value} onClick={() => handleClick(index)} />
-				))}
-			</main>
-		</Fragment>
+        <main className={`game-grid ${currentPlayer}`}>
+            {cells.map((value, index) => (
+                <Cell key={index} value={value} onClick={() => handleClick(index)} />
+            ))}
+        </main>
 	)
 }
 
--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -1,10 +1,44 @@
+import { Fragment, useState } from 'react'
+import { calculateWinner } from '../helpers'
 import Board from './Board'
 
 function Game() {
-    const [history, setHistory] = useState(new Array(9).fill(null))
+    const [history, setHistory] = useState([{ cells: new Array(9).fill(null) }])
     const [currentPlayer, setCurrentPlayer] = useState('x')
-    
-	return <Board />
+    const { cells } = history[history.length - 1]
+    const winner = calculateWinner(cells)
+    const status = winner ? `Winner: ${winner.toUpperCase()}` : `Next player: ${currentPlayer.toUpperCase()}`
+
+    const handleClick = (index) => {
+		const newCells = [...cells]
+        if (winner || newCells[index]) return
+        newCells[index] = currentPlayer
+		setHistory([...history, { cells: newCells }])
+        setCurrentPlayer(currentPlayer === 'x' ? 'o' : 'x')
+	}
+
+    const goToMove = (move) => {
+        console.log(move);
+    }
+
+	return (
+        <Fragment>
+			<div className="">
+                <p>{status}</p>
+                <ol>
+                    {history.map((step, move) => {
+                        const label = move ? `Go to mone #${move}`: `Go to game start`
+                        return (
+                            <li key={move}>
+                                <button onClick={() => goToMove(move)}>{label}</button>
+                            </li>
+                        )
+                    })}
+                </ol>
+            </div>
+			<Board cells={cells} currentPlayer={currentPlayer} handleClick={handleClick} />
+		</Fragment>
+    )
 }
 
 export default Game
